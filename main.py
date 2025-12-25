c8bb40f5aee4b25dc5d6b29705d35678faf81c9c
LayanDabour <Layandabour308@gmail.com>
2025-12-26 01:26:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import pandas as pd
+df = pd.read_csv('/content/parkinsons.csv')
+df = df.dropna()
+df.head()
 
+x = df[['PPE', 'HNR']]
+y = df['status']
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+x_scaled = scaler.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+from sklearn.svm import SVC
+svc = SVC()
+svc.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score
+y_pred = svc.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
